873e7c1981d022e601ae2b70bef6b0fdc25d2257
MariusLHL <59567003+MariusLHL@users.noreply.github.com>
2020-02-06 23:27:16 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,21 +13,17 @@ Stroll.destroy_all
 City.destroy_all
 
 10.times do
-  puts "t"
   City.create(name: Faker::Address.city)
 end
 
 23.times do
-  puts "e"
   Dog.create(name:Faker::Lorem.word, city_id: rand(City.first.id..City.last.id))
 end
 
 10.times do
-  puts "s"
   Dogsitter.create(first_name:Faker::Name.first_name , last_name:Faker::Name.last_name , city_id: rand(City.first.id..City.last.id))
 end
 
 50.times do
-  puts "t"
   Stroll.create(dogsitter_id: rand(Dogsitter.first.id..Dogsitter.last.id), dog_id: rand(Dog.first.id..Dog.last.id))
 end
